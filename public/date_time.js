950ab2877658d0b7e9db78b7c8927c4dab08d337
rmens <r.mens@me.com>
2016-11-29 19:30:12 +0100

    Static datum

---
+++ public/date_time.js
@@ -0,0 +1,29 @@
+function date_time(id)
+{
+        date = new Date;
+        year = date.getFullYear();
+        month = date.getMonth();
+        months = new Array('januari', 'febuari', 'maart', 'april', 'mei', 'juni', 'july', 'augutus', 'september', 'oktober', 'november', 'decemer');
+        d = date.getDate();
+        day = date.getDay();
+        days = new Array('zondag', 'maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag');
+        h = date.getHours();
+        if(h<10)
+        {
+                h = "0"+h;
+        }
+        m = date.getMinutes();
+        if(m<10)
+        {
+                m = "0"+m;
+        }
+        s = date.getSeconds();
+        if(s<10)
+        {
+                s = "0"+s;
+        }
+        result = ''+days[day]+' '+months[month]+' '+d+' '+year+' '+h+':'+m+':'+s;
+        document.getElementById(id).innerHTML = result;
+        setTimeout('date_time("'+id+'");','1000');
+        return true;
+}
